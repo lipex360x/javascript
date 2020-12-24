d569e4cff952cc5ef94dce4e6ac0c9bfb30e730d
Felipe Borges <lipex360@gmail.com>
2020-12-24 20:44:58 -0300

    Axios JSON Async/Await

--- asynchronous/03 - axios/assets/js/main.js
+++ asynchronous/03 - axios/assets/js/main.js
@@ -1,30 +1,37 @@
 const $ = document.querySelector.bind(document);
 
-fetch("pessoas.json")
-  .then((response) => response.json())
-  .then(async (json) => await loadElements(json));
+async function loadJson() {
+  try {
+    const people = await axios("pessoas.json");
+    return loadElements(people.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function loadElements(json) {
-  const table = document.createElement('table')
+  const table = document.createElement("table");
 
   for (let people of json) {
-    const tr = document.createElement('tr')
+    const tr = document.createElement("tr");
 
-    let td1 = document.createElement('td')
-    td1.innerHTML = people.nome
-    tr.appendChild(td1)
+    let td1 = document.createElement("td");
+    td1.innerHTML = people.nome;
+    tr.appendChild(td1);
 
-    let td2 = document.createElement('td')
-    td2.innerHTML = people.estado
-    tr.appendChild(td2)
+    let td2 = document.createElement("td");
+    td2.innerHTML = people.estado;
+    tr.appendChild(td2);
 
-    let td3 = document.createElement('td')
-    td3.innerHTML = people.salario
-    tr.appendChild(td3)
+    let td3 = document.createElement("td");
+    td3.innerHTML = people.salario;
+    tr.appendChild(td3);
 
-    table.appendChild(tr)
+    table.appendChild(tr);
   }
 
-  const result = $('.result')
-  result.appendChild(table)
+  const result = $(".result");
+  result.appendChild(table);
 }
+
+loadJson();
